ba7ced7af7b6d41cc67cac47072688bed9dd24ba
Carot <174653796+ILiekCarot@users.noreply.github.com>
2024-09-25 09:32:06 +0200

    AAAAAAAAAAAAAAAAAA
    im retarded

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,5 @@
 local linkToZip = "https://nightly.link/Carot-Subsidiaries/Flourine/workflows/build/master/Latest.zip"
 
-local zzlib = loadstring(game:HttpGet("https://github.com/Carot-Subsidiaries/loadZip/raw/refs/heads/master/inflate-bwo.lua"), "loadZip")()
+local zzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Carot-Subsidiaries/loadZip/refs/heads/master/zzlib.lua"), "loadZip")()
 
 loadstring(zzlib.unzip(linkToZip, "flourine.lua"), "Flourine")()
