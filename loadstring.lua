0070e438d43eb97ffb110c74afc1340a307fb546
Carot <174653796+ILiekCarot@users.noreply.github.com>
2024-10-01 01:03:48 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,5 @@
-local linkToZip = "https://nightly.link/Carot-Subsidiaries/Flourine/workflows/build/master/Latest.zip"
+local zip = game:HttpGet("https://nightly.link/Carot-Subsidiaries/Flourine/workflows/build/master/Latest.zip", true)
 
 local zzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Carot-Subsidiaries/loadZip/refs/heads/master/zzlib.lua"), "loadZip")()
 
-loadstring(zzlib.unzip(linkToZip, "flourine.lua"), "Flourine")()
+loadstring(zzlib.unzip(zip, "flourine.lua"), "Flourine")()
