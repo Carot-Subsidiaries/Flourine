c23ecea397870deb19af013e20b447b6c651397a
Carot <174653796+ILiekCarot@users.noreply.github.com>
2024-10-02 06:51:04 +0200

    ohhh + changes (the {"\n"} was because i was unsure if \n worked in the ``)

--- modules/ScriptViewer.lua
+++ modules/ScriptViewer.lua
@@ -95,92 +95,94 @@ local function main()
 		dumpbtn.TextColor3 = Color3.new(1,1,1)
 
 		dumpbtn.MouseButton1Click:Connect(function()
+            local dumpFunc = function()
+                -- thanks King.Kevin#6025 you'll obviously be credited (no discord tag since that can easily be impersonated)
+                local getgc = getgc or get_gc_objects
+                local getupvalues = (debug and debug.getupvalues) or getupvalues or getupvals
+                local getconstants = (debug and debug.getconstants) or getconstants or getconsts
+                local getinfo = (debug and (debug.getinfo or debug.info)) or getinfo
+                local original = ("\n-- // Function Dumper made by King.Kevin\n-- // Script Path: %s\n\n--[["):format(PreviousScr:GetFullName())
+                local dump = original
+                local functions, function_count, data_base = {}, 0, {}
+                function functions:add_to_dump(str, indentation, new_line)
+                    local new_line = new_line or true
+                    dump = dump .. ("%s%s%s"):format(string.rep("    ", indentation), tostring(str), new_line and "\n" or "")
+                end
+                function functions:get_function_name(func)
+                    local n = getinfo(func).name
+                    return n ~= "" and n or "Unknown Name"
+                end
+                function functions:dump_table(input, indent, index)
+                    local indent = indent < 0 and 0 or indent
+                    functions:add_to_dump(("%s [%s] %s"):format(tostring(index), tostring(typeof(input)), tostring(input)), indent - 1)
+                    local count = 0
+                    for index, value in pairs(input) do
+                        count = count + 1
+                        if type(value) == "function" then
+                            functions:add_to_dump(("%d [function] = %s"):format(count, functions:get_function_name(value)), indent)
+                        elseif type(value) == "table" then
+                            if not data_base[value] then
+                                data_base[value] = true
+                                functions:add_to_dump(("%d [table]:"):format(count), indent)
+                                functions:dump_table(value, indent + 1, index)
+                            else
+                                functions:add_to_dump(("%d [table] (Recursive table detected)"):format(count), indent)
+                            end
+                        else
+                            functions:add_to_dump(("%d [%s] = %s"):format(count, tostring(typeof(value)), tostring(value)), indent)
+                        end
+                    end
+                end
+                function functions:dump_function(input, indent)
+                    functions:add_to_dump(("\nFunction Dump: %s"):format(functions:get_function_name(input)), indent)
+                    functions:add_to_dump(("\nFunction Upvalues: %s"):format(functions:get_function_name(input)), indent)
+                    for index, upvalue in pairs(getupvalues(input)) do
+                        if type(upvalue) == "function" then
+                            functions:add_to_dump(("%d [function] = %s"):format(index, functions:get_function_name(upvalue)), indent + 1)
+                        elseif type(upvalue) == "table" then
+                            if not data_base[upvalue] then
+                                data_base[upvalue] = true
+                                functions:add_to_dump(("%d [table]:"):format(index), indent + 1)
+                                functions:dump_table(upvalue, indent + 2, index)
+                            else
+                                functions:add_to_dump(("%d [table] (Recursive table detected)"):format(index), indent + 1)
+                            end
+                        else
+                            functions:add_to_dump(("%d [%s] = %s"):format(index, tostring(typeof(upvalue)), tostring(upvalue)), indent + 1)
+                        end
+                    end
+                    functions:add_to_dump(("\nFunction Constants: %s"):format(functions:get_function_name(input)), indent)
+                    for index, constant in pairs(getconstants(input)) do
+                        if type(constant) == "function" then
+                            functions:add_to_dump(("%d [function] = %s"):format(index, functions:get_function_name(constant)), indent + 1)
+                        elseif type(constant) == "table" then
+                            if not data_base[constant] then
+                                data_base[constant] = true
+                                functions:add_to_dump(("%d [table]:"):format(index), indent + 1)
+                                functions:dump_table(constant, indent + 2, index)
+                            else
+                                functions:add_to_dump(("%d [table] (Recursive table detected)"):format(index), indent + 1)
+                            end
+                        else
+                            functions:add_to_dump(("%d [%s] = %s"):format(index, tostring(typeof(constant)), tostring(constant)), indent + 1)
+                        end
+                    end
+                end
+                for _, _function in pairs(getgc()) do
+                    if typeof(_function) == "function" and getfenv(_function).script and getfenv(_function).script == PreviousScr then
+                        functions:dump_function(_function, 0)
+                        functions:add_to_dump("\n" .. ("="):rep(100), 0, false)
+                    end
+                end
+                local source = codeFrame:GetText()
+                if dump ~= original then source = source .. dump .. "]]" end
+                codeFrame:SetText(source)
+            end
 			if PreviousScr ~= nil and not PreviousScr:IsA("ModuleScript") then
-				pcall(function()
-                    -- thanks King.Kevin#6025 you'll obviously be credited (no discord tag since that can easily be impersonated)
-                    local getgc = getgc or get_gc_objects
-                    local getupvalues = (debug and debug.getupvalues) or getupvalues or getupvals
-                    local getconstants = (debug and debug.getconstants) or getconstants or getconsts
-                    local getinfo = (debug and (debug.getinfo or debug.info)) or getinfo
-                    local original = ("\n-- // Function Dumper made by King.Kevin\n-- // Script Path: %s\n\n--[["):format(PreviousScr:GetFullName())
-                    local dump = original
-                    local functions, function_count, data_base = {}, 0, {}
-                    function functions:add_to_dump(str, indentation, new_line)
-                        local new_line = new_line or true
-                        dump = dump .. ("%s%s%s"):format(string.rep("    ", indentation), tostring(str), new_line and "\n" or "")
-                    end
-                    function functions:get_function_name(func)
-                        local n = getinfo(func).name
-                        return n ~= "" and n or "Unknown Name"
-                    end
-                    function functions:dump_table(input, indent, index)
-                        local indent = indent < 0 and 0 or indent
-                        functions:add_to_dump(("%s [%s] %s"):format(tostring(index), tostring(typeof(input)), tostring(input)), indent - 1)
-                        local count = 0
-                        for index, value in pairs(input) do
-                            count = count + 1
-                            if type(value) == "function" then
-                                functions:add_to_dump(("%d [function] = %s"):format(count, functions:get_function_name(value)), indent)
-                            elseif type(value) == "table" then
-                                if not data_base[value] then
-                                    data_base[value] = true
-                                    functions:add_to_dump(("%d [table]:"):format(count), indent)
-                                    functions:dump_table(value, indent + 1, index)
-                                else
-                                    functions:add_to_dump(("%d [table] (Recursive table detected)"):format(count), indent)
-                                end
-                            else
-                                functions:add_to_dump(("%d [%s] = %s"):format(count, tostring(typeof(value)), tostring(value)), indent)
-                            end
-                        end
-                    end
-                    function functions:dump_function(input, indent)
-                        functions:add_to_dump(("\nFunction Dump: %s"):format(functions:get_function_name(input)), indent)
-                        functions:add_to_dump(("\nFunction Upvalues: %s"):format(functions:get_function_name(input)), indent)
-                        for index, upvalue in pairs(getupvalues(input)) do
-                            if type(upvalue) == "function" then
-                                functions:add_to_dump(("%d [function] = %s"):format(index, functions:get_function_name(upvalue)), indent + 1)
-                            elseif type(upvalue) == "table" then
-                                if not data_base[upvalue] then
-                                    data_base[upvalue] = true
-                                    functions:add_to_dump(("%d [table]:"):format(index), indent + 1)
-                                    functions:dump_table(upvalue, indent + 2, index)
-                                else
-                                    functions:add_to_dump(("%d [table] (Recursive table detected)"):format(index), indent + 1)
-                                end
-                            else
-                                functions:add_to_dump(("%d [%s] = %s"):format(index, tostring(typeof(upvalue)), tostring(upvalue)), indent + 1)
-                            end
-                        end
-                        functions:add_to_dump(("\nFunction Constants: %s"):format(functions:get_function_name(input)), indent)
-                        for index, constant in pairs(getconstants(input)) do
-                            if type(constant) == "function" then
-                                functions:add_to_dump(("%d [function] = %s"):format(index, functions:get_function_name(constant)), indent + 1)
-                            elseif type(constant) == "table" then
-                                if not data_base[constant] then
-                                    data_base[constant] = true
-                                    functions:add_to_dump(("%d [table]:"):format(index), indent + 1)
-                                    functions:dump_table(constant, indent + 2, index)
-                                else
-                                    functions:add_to_dump(("%d [table] (Recursive table detected)"):format(index), indent + 1)
-                                end
-                            else
-                                functions:add_to_dump(("%d [%s] = %s"):format(index, tostring(typeof(constant)), tostring(constant)), indent + 1)
-                            end
-                        end
-                    end
-                    for _, _function in pairs(getgc()) do
-                        if typeof(_function) == "function" and getfenv(_function).script and getfenv(_function).script == PreviousScr then
-                            functions:dump_function(_function, 0)
-                            functions:add_to_dump("\n" .. ("="):rep(100), 0, false)
-                        end
-                    end
-                    local source = codeFrame:GetText()
-                    if dump ~= original then source = source .. dump .. "]]" end
-                    codeFrame:SetText(source)
-                end)
+				pcall(dumpFunc)
             else
-                codeFrame:SetText(`return game:GetService("HttpService"):JsonDecode({game:GetService("HttpService"):JsonEncode(require)})`)
+                codeFrame:SetText(`--This return is simulated, function dump will be provided below.{"\n"}return game:GetService("HttpService"):JSONDecode({cloneref(game:GetService("HttpService")):JSONEncode(require)}`)
+                pcall(dumpFunc)
             end
 		end)
 	end
